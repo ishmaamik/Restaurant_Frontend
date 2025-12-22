688ef85ae2852891fc7d4d65f75dc0824d7962ce
Ishmaam Iftekhar Khan <ishmaam@iut-dhaka.edu>
2025-12-22 15:31:57 +0600

    add: menu items

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    domains: ["images.squarespace-cdn.com"], // add any other external domains you need
+  },
+};
+
+export default nextConfig;
