e2b9df622c4421fa157edc320bb7f5c4c9f1cf85
Ishmaam Iftekhar Khan <ishmaam@iut-dhaka.edu>
2026-01-03 02:18:07 +0600

    Background of menu list set

--- app/menu/list/page.tsx
+++ app/menu/list/page.tsx
@@ -17,35 +17,39 @@ export default function Menu() {
     }, [])
 
     return (
-        <div className=" grid grid-rows-6 p-20 bg-green-10">
-            {
-                menuItems.map((item, index) => {
-                    
-                    const fromLeft= index%2==0
-                    
-                    return (
+        <div className='min-h-screen' style={{ backgroundImage: "url('/foodaura2.avif')", backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center', backgroundAttachment: 'fixed', }}>
 
-                        <motion.div
+            <div className="min-h-screen inset-0 bg-black/50 backdrop-blur-sm">
+                <div className=" grid grid-rows-6 p-20 bg-green-10">
+                    {
+                        menuItems.map((item, index) => {
 
-                            transition={{ease:'easeIn', duration:0.5}}
-                            whileInView={{opacity:1, x:0}}  //after animation ends, it will exactly be where it is intended in the classname
-                            //animate={{opacity:1, x:0}} for animations above and below screens too
-                            initial={{opacity:0, x: fromLeft ? -60 : 60}}
-                            
-                            key={item.menuId} 
-                            className={`flex ${index % 2 == 0 ? `justify-start` : `justify-end`}`}
-                        >
-                            <div className={`${fromLeft ?  `pl-40` : 'pr-40'} relative p-4 w-100 bg-green-50 rounded-xl shadow`}>
-                                <p className='text-3xl'>{item.name}</p>
-                                <motion.img width={200} initial={{opacity:0, x: fromLeft ? -60 : 60}} whileInView={{opacity:1, x:0}} transition={{duration:2, ease:'easeIn'}} viewport={{ once: false }} src={item.imageURL || null} alt={item.name} className='absolute rounded-xl'/>
-                                <p className='text-xl pt-50'>{item.price}</p>
-                                <p>{item.category}</p>
-                            </div>
-                        </motion.div>
-                    )
-                })
+                            const fromLeft = index % 2 == 0
 
-            }
+                            return (
+                                <motion.div
+
+                                    transition={{ ease: 'easeIn', duration: 0.5 }}
+                                    whileInView={{ opacity: 1, x: 0 }}  //after animation ends, it will exactly be where it is intended in the classname
+                                    //animate={{opacity:1, x:0}} for animations above and below screens too
+                                    initial={{ opacity: 0, x: fromLeft ? -60 : 60 }}
+
+                                    key={item.menuId}
+                                    className={`flex ${index % 2 == 0 ? `justify-start` : `justify-end`}`}
+                                >
+                                    <div className={`${fromLeft ? `pl-20` : 'pr-20'} relative p-4 w-100 bg-green-50 rounded-xl shadow`}>
+                                        <p className='text-3xl'>{item.name}</p>
+                                        <motion.img width={200} initial={{ opacity: 0, x: fromLeft ? -60 : 60 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 2, ease: 'easeIn' }} viewport={{ once: false }} src={item.imageURL || null} alt={item.name} className='absolute rounded-xl' />
+                                        <p className='text-xl pt-50'>{item.price}</p>
+                                        <p>{item.category}</p>
+                                    </div>
+                                </motion.div>
+                            )
+                        })
+
+                    }
+                </div>
+            </div>
         </div>
     )
 }
